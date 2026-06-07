528487fc64f6896d6924824ed6680061f9af7018
byaruhangaonesmus77-cloud <byaruhangaonesmus77@gmail.com>
2026-06-07 21:29:35 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -17,3 +17,10 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+<script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(() => console.log('Service Worker Registered'))
+      .catch(err => console.error('Service Worker failed:', err));
+  }
+</script>
